a16ddf868a43a5a94298fcba274e10cda919f3e1
nastya-zz <zelenenkaya9@gmail.com>
2021-02-02 23:59:13 +0300

    init mock for films and genres menu

---
+++ src/mocks/films.js
@@ -0,0 +1,30 @@
+export const films = [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    img: `img/bohemian-rhapsody.jpg`
+  },
+  {
+    title: `Macbeth`,
+    img: `img/macbeth.jpg`
+  },
+  {
+    title: `Aviator`,
+    img: `img/aviator.jpg`
+  },
+  {
+    title: `We need to talk about Kevin`,
+    img: `img/we-need-to-talk-about-kevin.jpg`
+  },
+  {
+    title: `What We Do in the Shadows`,
+    img: `img/what-we-do-in-the-shadows.jpg`
+  },
+  {
+    title: `Revenant`,
+    img: `img/revenant.jpg`
+  },
+];
