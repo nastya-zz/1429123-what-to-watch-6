2f89ec945d4793c3fb2e1d0f21f5c77f8edee2d9
nastya-zz <zelenenkaya9@gmail.com>
2021-02-06 15:44:51 +0300

    add route components

---
+++ src/components/sign-in/sign-in-message.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+const SignInMessage = (props) => {
+  return (
+    <>
+      <div className="sign-in__message">
+        <p>Please enter a valid email address</p>
+      </div>
+    </>
+  );
+};
+
+export default SignInMessage;
