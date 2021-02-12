5aac56099470c5b8a9b3f43f835d3710d2155c59
nastya-zz <zelenenkaya9@gmail.com>
2021-02-12 23:40:16 +0300

    add mock reviews

---
+++ src/mocks/review.js
@@ -0,0 +1,32 @@
+export const reviews = [
+  {
+    "id": 0,
+    "user": {
+      "id": 4,
+      "name": `Kate Muir`
+    },
+    "rating": 8.9,
+    "comment": `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 1,
+    "user": {
+      "id": 4,
+      "name": `Mulen Zorach`
+    },
+    "rating": 7.7,
+    "comment": `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 2,
+    "user": {
+      "id": 4,
+      "name": `Travis Ozarc`
+    },
+    "rating": 8.3,
+    "comment": `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    "date": `2019-05-08T14:13:56.569Z`
+  }
+];
