1a184502aaf6f025a21de3f4c57246e73078c940
nastya-zz <zelenenkaya9@gmail.com>
2021-02-22 19:18:49 +0300

    add playing preview video

---
+++ src/components/common/film/video-player.jsx
@@ -0,0 +1,36 @@
+import React, {createRef, useEffect} from "react";
+import PropTypes from "prop-types";
+
+const VideoPlayer = (props) => {
+  const {previewVideoLink, previewImage, isPlaying, ...restProps} = props;
+  const videoRef = createRef();
+
+  useEffect(() => {
+    if (isPlaying) {
+      videoRef.current.play();
+      return;
+    }
+
+    videoRef.current.pause();
+    videoRef.current.load();
+
+  }, [isPlaying]);
+
+  return (
+    <video
+      ref={videoRef}
+      src={previewVideoLink}
+      className="player__video"
+      poster={previewImage}
+      {...restProps}
+    />
+  );
+};
+
+VideoPlayer.propTypes = {
+  previewVideoLink: PropTypes.string.isRequired,
+  previewImage: PropTypes.string.isRequired,
+  isPlaying: PropTypes.bool.isRequired
+};
+
+export default VideoPlayer;
