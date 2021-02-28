5a4bfd0439829acaa66aade92ee3cea78f227c2d
nastya-zz <zelenenkaya9@gmail.com>
2021-02-28 13:26:02 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-
-import {Genre} from "./constants/genres";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {Genre} from "./mocks/genres";
 import {films} from "./mocks/films";
+import {reducer} from "./store/reducer";
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const genres = Object.values(Genre);
 
+const store = createStore(reducer, composeWithDevTools());
+
 ReactDOM.render(
-    <App
-      genres={genres}
-      films={films}
-    />,
+    <Provider store={store}>
+      <App
+        genres={genres}
+        films={films}
+      />
+    </Provider>,
     document.querySelector(`#root`),
 );
---
+++ src/store/reducer.js
@@ -0,0 +1,24 @@
+import {films} from "../mocks/films";
+import {Genre} from "../mocks/genres";
+import {ActionType} from "./action";
+
+const initialState = {
+  films,
+  genre: Genre.ALL
+};
+
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.SET_GENRE:
+      return {
+        ...state,
+        genre: action.payload
+      };
+  }
+
+  return state;
+};
+
+
+export {reducer};
