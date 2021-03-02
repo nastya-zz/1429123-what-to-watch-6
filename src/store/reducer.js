ff2dff8816ca066ac97b5293e29f6c40b373ff19
nastya-zz <zelenenkaya9@gmail.com>
2021-03-02 23:14:31 +0300

    add actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,11 +1,14 @@
 import {films} from "../mocks/films";
 import {ActionType} from "./action";
+import {FilmCount} from "../constants/common";
 
 export const genres = [`All genres`, ...new Set(films.map((film) => film.genre))];
 
 const initialState = {
   films,
-  genre: genres[0]
+  filteredFilmsByGenre: films,
+  genre: genres[0],
+  mainPageFilmCount: FilmCount.MAIN_PAGE
 };
 
 
@@ -14,7 +17,18 @@ const reducer = (state = initialState, action) => {
     case ActionType.SET_GENRE:
       return {
         ...state,
-        genre: action.payload
+        genre: action.payload,
+        filteredFilmsByGenre: action.payload === `All genres` ? state.films : state.films.filter((film) => film.genre === action.payload)
+      };
+    case ActionType.SHOW_MORE_FILMS:
+      return {
+        ...state,
+        mainPageFilmCount: state.mainPageFilmCount + FilmCount.MAIN_PAGE
+      };
+    case ActionType.RESET_MAIN_FILMS_COUNT:
+      return {
+        ...state,
+        mainPageFilmCount: FilmCount.MAIN_PAGE
       };
   }
 
