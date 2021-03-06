7a44bb82c9ebe0361e165dd22a91f9bfc68f855c
nastya-zz <zelenenkaya9@gmail.com>
2021-03-06 18:27:12 +0300

    add consts

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,5 +1,11 @@
 export const PLAYING_DELAY = 1000;
+export const DEFAULT_GENRE = `All genres`;
 export const FilmCount = {
   MAIN_PAGE: 8,
   MY_LIST: 4
+};
+
+export const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
 };
