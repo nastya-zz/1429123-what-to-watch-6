2637cd462027e984ff0f11fb1894796ba753d56c
nastya-zz <zelenenkaya9@gmail.com>
2021-03-06 18:23:38 +0300

    add actions

--- src/store/action.js
+++ src/store/action.js
@@ -1,7 +1,10 @@
 export const ActionType = {
   SET_GENRE: `main/setGenre`,
   SHOW_MORE_FILMS: `main/showMoreFilms`,
-  RESET_MAIN_FILMS_COUNT: `main/resetMainFilmsCount`
+  RESET_MAIN_FILMS_COUNT: `main/resetMainFilmsCount`,
+  REQUIRED_AUTHORIZATION: `user/requiredAuthorization`,
+  LOAD_FILMS: `data/loadFilms`,
+  SET_GENRES: `data/setGenres`
 };
 
 
@@ -15,5 +18,17 @@ export const ActionCreator = {
   }),
   resetMainFilmsCount: () => ({
     type: ActionType.RESET_MAIN_FILMS_COUNT
+  }),
+  requireAuthorization: (status) => ({
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: status,
+  }),
+  loadFilms: (films) => ({
+    type: ActionType.LOAD_FILMS,
+    payload: films
+  }),
+  setGenres: (genres) => ({
+    type: ActionType.SET_GENRES,
+    payload: genres
   })
 };
