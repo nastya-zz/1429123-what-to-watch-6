9ed2992d3369a15d6a0ccdf833386be742ff83f2
nastya-zz <zelenenkaya9@gmail.com>
2021-03-08 12:26:53 +0300

    add login

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import Footer from "../common/footer/footer";
 import LogoLink from '../common/logo/logo-link';
+import {useRef} from "react";
+import {useDispatch} from "react-redux";
+import {login} from "../../store/api-actions";
+import {useHistory} from "react-router-dom";
 
 
 const SignIn = () => {
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const email = loginRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (!email || !password) {
+      return;
+    }
+
+    dispatch(login({email, password}));
+    history.push(`/`);
+  };
+
   return (
     <>
       <div className="user-page">
@@ -14,15 +38,15 @@ const SignIn = () => {
         </header>
 
         <div className="sign-in user-page__content">
-          <form action="#" className="sign-in__form">
+          <form className="sign-in__form" onSubmit={handleSubmit}>
             <div className="sign-in__fields">
               <div className="sign-in__field">
-                <input className="sign-in__input" type="email" placeholder="Email address" name="user-email"
+                <input ref={loginRef} required className="sign-in__input" type="email" placeholder="Email address" name="user-email"
                   id="user-email"/>
                 <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
               </div>
               <div className="sign-in__field">
-                <input className="sign-in__input" type="password" placeholder="Password" name="user-password"
+                <input ref={passwordRef} required className="sign-in__input" type="password" placeholder="Password" name="user-password"
                   id="user-password"/>
                 <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
               </div>
--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -17,7 +17,7 @@ export const checkAuth = () => (dispatch, _getState, api) => (
     .catch(() => {})
 );
 
-export const login = ({login: email, password}) => (dispatch, _getState, api) => (
+export const login = ({email, password}) => (dispatch, _getState, api) => (
   api.post(`/login`, {email, password})
     .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
 );
