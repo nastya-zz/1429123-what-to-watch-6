222e4ab0e2db071fe14222f711b333212c9ea5d3
nastya-zz <zelenenkaya9@gmail.com>
2021-03-13 19:14:07 +0300

    add formatters

--- src/utils/film.js
+++ src/utils/film.js
@@ -8,3 +8,33 @@
 export const findFilmById = (id, films) => {
   return films.find((movie) => movie.id === parseInt(id, 10));
 };
+
+/**
+ * длительность фильма
+ * @param {number} minutes
+ * @return {string}
+ */
+export const getFilmDuration = (minutes) => {
+  if (minutes > 60) {
+    const hours = Math.trunc(minutes / 60);
+    minutes = Math.floor((((minutes / 60 - hours) * 60) / 100) * 100);
+
+    return `${hours}h ${minutes}m`;
+  } else {
+    return `${minutes}m`;
+  }
+};
+
+/**
+ *  форматирование даты
+ * @param {string} dateLong
+ * @return {string}
+ */
+export const formatDate = (dateLong) => {
+  const options = {
+    month: `long`,
+    day: `numeric`,
+    year: `numeric`
+  };
+  return dateLong ? new Intl.DateTimeFormat(`en-US`, options).format(new Date(dateLong)) : ``;
+};
