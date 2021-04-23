befd338f344f27ece31271bb7c40a4e60f3d1312
nastya-zz <zelenenkaya9@gmail.com>
2021-04-24 00:16:43 +0300

    add test for async reducers

--- src/store/film/film.test.js
+++ src/store/film/film.test.js
@@ -1,12 +1,22 @@
 import {film} from './film';
+import MockAdapter from "axios-mock-adapter";
 import {
+  ActionType,
   loadFavoriteList,
   loadFilmById,
   loadFilms,
-  loadReviewList, setPromoFilm,
+  loadReviewList, setGenre, setGenres, setPromoFilm,
   setSelectedFilmByIdLoaded,
   setShowReviewErrorMsg,
 } from "../action";
+import {createAPI} from "../../api";
+import {ApiRoute, DEFAULT_GENRE} from "../../constants/common";
+import {addToFavoriteList, fetchFavoriteFilmList, fetchFilmList, fetchReviewList} from "../api-actions";
+import {films} from "../../mocks/films";
+import {getAdaptedFilm} from "../../utils/adapters";
+import {reviews} from "../../mocks/review";
+
+const api = createAPI(() => {});
 
 describe(`Test film reducer`, () => {
   it(`Reducer without additional parameters should return initial state`, () => {
@@ -117,6 +127,64 @@ describe(`Test film reducer`, () => {
     expect(film(state, setPromoFilm(promoFilm)))
       .toEqual({
         promoFilm: {name: `Some promo film`}
+      });
+  });
+});
+
+describe(`Async operations work correctly`, () => {
+  it(`Should make a correct API call to /films`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const filmLoader = fetchFilmList();
+
+    apiMock
+      .onGet(ApiRoute.FILMS)
+      .reply(200, films);
+
+    const genres = [DEFAULT_GENRE, ...new Set(films.map((movie) => movie.genre))];
+    const adaptedFilmList = films.map((movie) => getAdaptedFilm(movie));
+
+    return filmLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(3);
+
+        expect(dispatch).toHaveBeenNthCalledWith(1, loadFilms(adaptedFilmList));
+        expect(dispatch).toHaveBeenNthCalledWith(2, setGenres(genres));
+        expect(dispatch).toHaveBeenNthCalledWith(3, setGenre(DEFAULT_GENRE));
+      });
+  });
+
+  it(`Should make a correct API call to /favorite`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const favoriteFilmLoader = fetchFavoriteFilmList();
+
+    apiMock
+      .onGet(ApiRoute.FAVORITE)
+      .reply(200, films);
+
+    const adaptedFilmList = films.map((movie) => getAdaptedFilm(movie));
+
+    return favoriteFilmLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, loadFavoriteList(adaptedFilmList));
+      });
+  });
+
+  it(`Should make a correct API call to /comments`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fetchReviewListLoader = fetchReviewList(1);
+
+    apiMock
+      .onGet(`${ApiRoute.COMMENTS}/1`)
+      .reply(200, reviews);
+
+    return fetchReviewListLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, loadReviewList(reviews));
       });
   });
 });
